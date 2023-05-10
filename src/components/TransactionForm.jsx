5f94da44363ce4e488a1458819f151dc4b2c2190
Zekret <benjaminconcha5@gmail.com>
2023-05-10 12:15:33 -0400

    update: state add transaction

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,7 +1,34 @@
-const TransactionForm = () => {
-  return (
-    <div>TransactionForm</div>
-  )
-}
+import { useState } from "react";
+import { useGlobalState } from '../context/GlobalState'
 
-export default TransactionForm
+const TransactionForm = () => {
+  const { addTransaction } = useGlobalState()
+  const [description, setDescription] = useState();
+  const [amount, setAmount] = useState(0);
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    addTransaction({ id: 1, description, amount })
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Ingresa una descripcion"
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <input
+          type="number"
+          step="0.01"
+          placeholder="00.00"
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button>Agrega una transaccion</button>
+      </form>
+    </div>
+  );
+};
+
+export default TransactionForm;
